b4846b10a67994b3eca563ebe2dada929bf6ebbd
Harmanjit Singh <57390891+harman20@users.noreply.github.com>
2020-07-29 13:22:04 +0530

    Add files via upload

---
+++ JavaFinalMethod.java
@@ -0,0 +1,12 @@
+class JavaFinalMethod{  
+  final void run(){System.out.println("running");}  
+}  
+     
+class Honda extends JavaFinalMethod{  
+   void run(){System.out.println("running safely with 100kmph");}  
+     
+   public static void main(String args[]){  
+   Honda honda= new Honda();  
+   honda.run();  
+   }  
+}  
---
+++ finalvariable.java
@@ -0,0 +1,9 @@
+class finalvariable{  
+ final int speedlimit=90;
+ void run(){  
+  speedlimit=400;  
+ }  
+ public static void main(String args[]){  
+ Bike9 obj=new  Bike9();  
+ obj.run();  
+ }  
